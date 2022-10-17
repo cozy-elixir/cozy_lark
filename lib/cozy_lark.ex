fdfc41fae3979882c1f58fc9a2210352babc7f46
c4710n <c4710n@users.noreply.github.com>
2022-10-17 10:39:49 +0800

    refactor: clean `CozyLark`

--- lib/cozy_lark.ex
+++ lib/cozy_lark.ex
@@ -2,17 +2,4 @@ defmodule CozyLark do
   @moduledoc """
   Documentation for `CozyLark`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> CozyLark.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/cozy_lark_test.exs
+++ test/cozy_lark_test.exs
@@ -1,8 +1,4 @@
 defmodule CozyLarkTest do
   use ExUnit.Case
   doctest CozyLark
-
-  test "greets the world" do
-    assert CozyLark.hello() == :world
-  end
 end

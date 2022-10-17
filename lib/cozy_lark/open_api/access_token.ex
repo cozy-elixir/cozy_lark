69ab1726c9cae64e7d143efc270e2351cfa71f40
c4710n <c4710n@users.noreply.github.com>
2022-10-17 19:45:49 +0800

    refactor: adjust `CozyLark.OpenAPI.AccessToken` based on new config

--- lib/cozy_lark/open_api/access_token.ex
+++ lib/cozy_lark/open_api/access_token.ex
@@ -7,36 +7,36 @@ defmodule CozyLark.OpenAPI.AccessToken do
   """
 
   alias CozyLark.HTTPClient
+  alias CozyLark.OpenAPI.Config
   alias CozyLark.OpenAPI.Domain
 
   @supported_api %{
     {:custom_app, :tenant_access_token} => %{
       method: :post,
-      path: "/auth/v3/tenant_access_token/internal",
-      body_required_keys: [:app_id, :app_secret]
+      path: "/auth/v3/tenant_access_token/internal"
     },
     {:custom_app, :app_access_token} => %{
       method: :post,
-      path: "/auth/v3/app_access_token/internal",
-      body_required_keys: [:app_id, :app_secret]
+      path: "/auth/v3/app_access_token/internal"
     }
   }
 
   # TODO: add cache
-  def get_access_token(domain, app_type, access_token_type, body)
-      when app_type in [:custom_app] and
-             access_token_type in [:tenant_access_token, :app_access_token] and
-             is_map(body) do
-    %{method: method, path: path, body_required_keys: body_required_keys} =
-      Map.fetch!(@supported_api, {app_type, access_token_type})
+  def get_access_token(
+        %Config{
+          app_id: app_id,
+          app_secret: app_secret,
+          app_type: :custom_app = app_type,
+          domain: domain
+        },
+        access_token_type
+      )
+      when access_token_type in [:tenant_access_token, :app_access_token] do
+    %{method: method, path: path} = fetch_api_info!(app_type, access_token_type)
+    url = Domain.build_url!(domain, path)
+    body = %{app_id: app_id, app_secret: app_secret}
 
-    url =
-      domain
-      |> Domain.fetch_base_url!()
-      |> Path.join(path)
-
-    with :ok <- check_request_body(body, body_required_keys),
-         {:ok, response} <- HTTPClient.request_json(method, url, %{}, %{}, body),
+    with {:ok, response} <- HTTPClient.request_json(method, url, %{}, %{}, body),
          %{"code" => code} <- response do
       if code == 0 do
         access_token = Map.fetch!(response, to_string(access_token_type))
@@ -47,11 +47,7 @@ defmodule CozyLark.OpenAPI.AccessToken do
     end
   end
 
-  def check_request_body(body, required_keys) do
-    if Enum.all?(required_keys, &Map.has_key?(body, &1)) do
-      :ok
-    else
-      {:error, {:access_token, :bad_body}}
-    end
+  defp fetch_api_info!(app_type, access_token_type) do
+    Map.fetch!(@supported_api, {app_type, access_token_type})
   end
 end

62546604b5ed944c1620f10deffaa9f9672cbe80
c4710n <c4710n@users.noreply.github.com>
2022-10-17 21:24:05 +0800

    chore: prepare for publishing this package

--- mix.exs
+++ mix.exs
@@ -1,13 +1,26 @@
 defmodule CozyLark.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @source_url "https://github.com/cozy-elixir/cozy_lark"
+  @description "Elixir SDK of Lark Open Platform."
+
   def project do
     [
       app: :cozy_lark,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      # doc
+      description: @description,
+      source_url: @source_url,
+      homepage_url: @source_url,
+      docs: docs(),
+      # package
+      package: package(),
+      # aliases
+      aliases: aliases()
     ]
   end
 
@@ -22,7 +35,35 @@ defmodule CozyLark.MixProject do
     [
       {:jason, ">= 1.0.0"},
       {:finch, "~> 0.13"},
-      {:con_cache, "~> 1.0"}
+      {:con_cache, "~> 1.0"},
+      {:ex_doc, "~> 0.25", only: :dev, runtime: false}
     ]
+  end
+
+  defp docs do
+    [
+      extras: ["README.md"],
+      main: "readme",
+      source_url: @source_url,
+      source_ref: @version
+    ]
+  end
+
+  defp package do
+    [
+      exclude_patterns: [],
+      licenses: ["Apache-2.0"],
+      links: %{GitHub: @source_url}
+    ]
+  end
+
+  defp aliases do
+    [publish: ["hex.publish", "tag"], tag: &tag_release/1]
+  end
+
+  defp tag_release(_) do
+    Mix.shell().info("Tagging release as #{@version}")
+    System.cmd("git", ["tag", @version])
+    System.cmd("git", ["push", "--tags"])
   end
 end

4824006d38cfa433bfe24a73652e9c28907ac153
c4710n <c4710n@users.noreply.github.com>
2022-10-17 19:44:37 +0800

    feat: add `CozyLark.OpenAPI.Domain.build_url!`

--- lib/cozy_lark/open_api/domain.ex
+++ lib/cozy_lark/open_api/domain.ex
@@ -1,4 +1,10 @@
 defmodule CozyLark.OpenAPI.Domain do
+  def build_url!(domain, path) do
+    domain
+    |> fetch_base_url!()
+    |> Path.join(path)
+  end
+
   def fetch_base_url!(domain) do
     urls = %{
       lark: "https://open.larksuite.com/open-apis",

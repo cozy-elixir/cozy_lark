1f98c14e10481a5deea1ccfe624918f3cfe57a2b
c4710n <c4710n@users.noreply.github.com>
2022-11-27 16:45:13 +0800

    feat: add `CozyLark.ServerSideAPI.Spec`

---
+++ lib/cozy_lark/server_side_api/spec.ex
@@ -0,0 +1,116 @@
+defmodule CozyLark.ServerSideAPI.Spec do
+  @moduledoc """
+  Describes the specification of a server-side API.
+
+  Read more at
+  [Getting Started - Calling the API](https://open.feishu.cn/document/ukTMukTMukTM/uITNz4iM1MjLyUzM).
+  """
+
+  @enforce_keys [
+    :access_token_type,
+    :method,
+    :path,
+    :query,
+    :headers,
+    :body
+  ]
+
+  defstruct access_token_type: nil,
+            method: nil,
+            path: nil,
+            query: %{},
+            headers: %{},
+            body: nil
+
+  @typedoc """
+  The type of access token which is used by this API.
+  """
+  @type access_token_type() :: :app_access_token | :tenant_access_token | :user_access_token
+
+  @typedoc """
+  API method.
+  """
+  @type method() :: String.t()
+
+  @typedoc """
+  API path.
+  """
+  @type path() :: String.t()
+
+  @typedoc """
+  API query.
+  """
+  @type query() :: %{
+          optional(query_name :: String.t()) => query_value :: boolean() | number() | String.t()
+        }
+
+  @typedoc """
+  API headers.
+  """
+  @type headers() :: %{optional(header_name :: String.t()) => header_value :: String.t()}
+
+  @typedoc """
+  Optional API body.
+  """
+  @type body() :: map() | nil
+
+  @type config() :: %{
+          access_token_type: nil,
+          method: method(),
+          path: path(),
+          query: query(),
+          headers: headers(),
+          body: body()
+        }
+
+  @type t :: %__MODULE__{
+          access_token_type: nil,
+          method: method(),
+          path: path(),
+          query: query(),
+          headers: headers(),
+          body: body()
+        }
+
+  @spec build!(config()) :: t()
+  def build!(config) when is_map(config) do
+    config
+    |> validate_required_keys!()
+    |> validate_access_token_type!()
+    |> as_struct!()
+  end
+
+  defp validate_required_keys!(
+         %{
+           access_token_type: access_token_type,
+           method: method,
+           path: path
+         } = config
+       )
+       when is_atom(access_token_type) and is_binary(method) and is_binary(path) do
+    config
+  end
+
+  defp validate_required_keys!(_config) do
+    raise ArgumentError,
+          "key :access_token_type, :method, :path are required in a spec"
+  end
+
+  @supported_access_token_types [:app_access_token, :tenant_access_token, :user_access_token]
+  defp validate_access_token_type!(%{access_token_type: type} = config)
+       when type in @supported_access_token_types do
+    config
+  end
+
+  defp validate_access_token_type!(%{access_token_type: type}) do
+    raise ArgumentError,
+          "unknown value of key :access_token_type - #{inspect(type)}"
+  end
+
+  defp as_struct!(config) do
+    default_struct = __MODULE__.__struct__()
+    valid_keys = Map.keys(default_struct)
+    config = Map.take(config, valid_keys)
+    Map.merge(default_struct, config)
+  end
+end
---
+++ test/cozy_lark/server_side_api/spec_test.exs
@@ -0,0 +1,35 @@
+defmodule CozyLark.ServerSideAPI.SpecTest do
+  use ExUnit.Case
+  alias CozyLark.ServerSideAPI.Spec
+
+  describe "build!/1" do
+    test "creates an %Spec{} struct" do
+      assert %Spec{access_token_type: _, method: _, path: _, query: _, headers: _, body: _} =
+               Spec.build!(%{
+                 access_token_type: :app_access_token,
+                 method: "GET",
+                 path: "/"
+               })
+    end
+
+    test "raises ArgumentError when required keys are missing" do
+      assert_raise ArgumentError,
+                   "key :access_token_type, :method, :path are required in a spec",
+                   fn ->
+                     Spec.build!(%{})
+                   end
+    end
+
+    test "raises ArgumentError when access_token_type is unknown" do
+      assert_raise ArgumentError,
+                   "unknown value of key :access_token_type - :unknown_access_token",
+                   fn ->
+                     Spec.build!(%{
+                       access_token_type: :unknown_access_token,
+                       method: "GET",
+                       path: "/"
+                     })
+                   end
+    end
+  end
+end

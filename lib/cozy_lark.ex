ba2f548cb6a4b238ae009818829215820fa552f7
c4710n <c4710n@users.noreply.github.com>
2023-01-08 15:25:34 +0800

    docs: polish it

--- lib/cozy_lark.ex
+++ lib/cozy_lark.ex
@@ -2,10 +2,10 @@ defmodule CozyLark do
   @moduledoc """
   An SDK builder of Lark Open Platform / Feishu Open Platform.
 
-  Developing Lark / Feishu application is all about:
+  Developing Lark / Feishu applications is all about:
 
-  + server-side API calling
-  + event subscription
+  + calling server-side API
+  + subscribing events
 
   `CozyLark` provides support for above processes by following modules:
 
